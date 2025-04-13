08f814ba4d4e1b457ecd7f5c9e6922e53a8c8546
vineet-1401 <vineetsakpal.work@gmail.com>
2025-04-13 22:45:25 +0530

    .

--- main.py
+++ main.py
@@ -134,4 +134,4 @@ def calculate_ats_score(resume_json, job_description):
 # ---------------------- Main ----------------------
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
